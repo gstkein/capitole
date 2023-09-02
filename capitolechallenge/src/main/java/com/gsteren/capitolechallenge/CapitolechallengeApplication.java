5f04e61600397febaeb5ddb168c889917f7e495d
Guillermo Steren <gstkein@gmail.com>
2023-09-02 17:51:10 -0300

    Initial Commit

---
+++ capitolechallenge/src/main/java/com/gsteren/capitolechallenge/CapitolechallengeApplication.java
@@ -0,0 +1,13 @@
+package com.gsteren.capitolechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitolechallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapitolechallengeApplication.class, args);
+	}
+
+}
