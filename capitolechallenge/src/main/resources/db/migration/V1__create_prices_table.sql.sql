a758f1762e10ed9b7e00a0a31435802a024a42e8
Guillermo Steren <gstkein@gmail.com>
2023-09-03 13:41:17 -0300

    Added Flyway Scripts

--- capitolechallenge/src/main/resources/db/migration/V1__create_prices_table.sql.sql
+++ capitolechallenge/src/main/resources/db/migration/V1__create_prices_table.sql.sql
@@ -1,8 +1,11 @@
-CREATE TABLE IF NOT EXISTS `employee` (
-
-    `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `name` varchar(20),
-    `email` varchar(50),
-    `date_of_birth` timestamp
-
-)ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+CREATE TABLE prices (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    brand_id INT NOT NULL,
+    start_date TIMESTAMP NOT NULL,
+    end_date TIMESTAMP NOT NULL,
+    price_list INT NOT NULL,
+    product_id INT NOT NULL,
+    priority INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    curr VARCHAR(3) NOT NULL
+);
